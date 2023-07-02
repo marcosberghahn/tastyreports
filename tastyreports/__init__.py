e39a853e921323d0fd057b99e67be699e2429bb8
astromodder <22250203+astromodder@users.noreply.github.com>
2023-07-02 15:52:10 -0400

    starting to refactor

--- tastyreports/__init__.py
+++ tastyreports/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, session, redirect, url_for, escape, request
 from dotenv import load_dotenv
 from tastytrade.session import Session
+from tastytrade.account import Account
 import os
 
 # Load .env file
@@ -16,5 +17,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'changeit')
 
 tt_session = Session(TT_USERNAME, TT_PASSWORD, is_certification=TT_CERT_ENV)
+
+def get_account_numbers():
+    accounts = Account.get_accounts(tt_session)
+    return [account.account_number for account in accounts]
+
+account_numbers = get_account_numbers()
+
+@app.route('/set_account/<string:account_number>')
+def set_account(account_number=None):
+    if account_number in account_numbers:
+        session['current_account'] = account_number
+    else:
+        session['current_account'] = account_numbers[0]
+    return redirect(request.referrer or url_for('home'))
 
 from tastyreports.routes import reports
--- tastyreports/routes/reports.py
+++ tastyreports/routes/reports.py
@@ -1,51 +1,21 @@
 from datetime import datetime
 from flask import render_template, redirect, request, url_for, session
-from tastyreports import app, tt_session
-from tastytrade.account import Account
-
-def get_account_numbers():
-    accounts = Account.get_accounts(tt_session)
-    return [account.account_number for account in accounts]
-
-account_numbers = get_account_numbers()
+from tastyreports import app, tt_session, account_numbers
+from tastytrade.account import Account, AccountBalance, AccountBalanceSnapshot
 
 @app.route('/')
-@app.route('/account/')
-def index():
-    # Handle empty list of account numbers
-    default_account_number = account_numbers[0] if account_numbers else None
-
-    if default_account_number is None:
-        return 'No accounts available', 404
-    else:
-        session['current_account'] = default_account_number
-        return redirect(url_for('home'))
-
 @app.route('/home')
 def home():
     account_number = session.get('current_account')
-    # Check if 'current_account' is in session
+    if account_number is None or account_number not in account_numbers:
+        account_number = account_numbers[0]
+
     if account_number is None:
-        print("Error: 'current_account' not found in session.")
-        # Handle the error, e.g., redirect to login page or show an error message
-    else:
-        # Handles when account_number is None or not in account_numbers list
-        if not account_number or account_number not in account_numbers:
-            account_number = account_numbers[0] if account_numbers else None
-            session['current_account'] = account_number
+        return 'No accounts available', 404
 
-        if account_number is None:
-            return 'No accounts available', 404
-
-        return render_template('home.html', account_numbers=account_numbers, current_account=account_number)
-
-@app.route('/set_account/<string:account_number>')
-def set_account(account_number=None):
-    if account_number in account_numbers:
-        session['current_account'] = account_number
-    else:
-        session['current_account'] = account_numbers[0]
-    return redirect(request.referrer or url_for('home'))
+    account = Account.get_account(tt_session, account_number)
+    balances = account.get_balances(tt_session)
+    return render_template('home.html', account_numbers=account_numbers, current_account=account_number, account=account, balances=balances)
 
 @app.route('/pl_calendar')
 def pl_calendar(account_number=None, start_date=None, end_date=None):
