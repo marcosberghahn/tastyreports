1c84b5bea82f9b359365175df621ae4b4958e202
astromodder <22250203+astromodder@users.noreply.github.com>
2023-07-02 14:59:43 -0400

    env template

--- tastyreports/__init__.py
+++ tastyreports/__init__.py
@@ -9,7 +9,6 @@ load_dotenv()
 # Get API credentials from .env file
 TT_USERNAME = os.getenv('TT_USERNAME')
 TT_PASSWORD = os.getenv('TT_PASSWORD')
-TT_ACCOUNT = os.getenv('TT_ACCOUNT')
 TT_CERT_ENV = os.getenv('TT_CERT_ENV', False)
 
 app = Flask(__name__)
