ae085f745338c68b2b0268fabe609c1d0d5903cb
astromodder <22250203+astromodder@users.noreply.github.com>
2023-07-02 16:56:14 -0400

    filtered out stuff for the calendar

--- tastyreports/__init__.py
+++ tastyreports/__init__.py
@@ -24,12 +24,4 @@ def get_account_numbers():
 
 account_numbers = get_account_numbers()
 
-@app.route('/set_account/<string:account_number>')
-def set_account(account_number=None):
-    if account_number in account_numbers:
-        session['current_account'] = account_number
-    else:
-        session['current_account'] = account_numbers[0]
-    return redirect(request.referrer or url_for('home'))
-
-from tastyreports.routes import reports
+from tastyreports.routes import home, utils, pl_calendar, pl_chart
--- tastyreports/routes/home.py
+++ tastyreports/routes/home.py
@@ -16,16 +16,3 @@ def home():
     account = Account.get_account(tt_session, account_number)
     balances = account.get_balances(tt_session)
     return render_template('home.html', account_numbers=account_numbers, current_account=account_number, account=account, balances=balances)
-
-@app.route('/pl_calendar')
-def pl_calendar(account_number=None, start_date=None, end_date=None):
-    if account_number is None:
-        account_number = session.get('current_account')
-
-    if start_date is None:
-        start_date = datetime.today().replace(day=1)
-
-    if end_date is None:
-        end_date = datetime.today()
-
-    return render_template('pl_calendar.html', account_numbers=account_numbers, current_account=account_number, start_date=start_date, end_date=end_date)
